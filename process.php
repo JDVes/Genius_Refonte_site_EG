2e41c239112bab8dede0f118df09d746f70d990b
iMac G1 <imacg1@iMac-de-Energy-20.local>
2022-06-29 11:48:41 +0000

    All brancjes merge

--- process.php
+++ process.php
@@ -1,3 +1,36 @@
 <?php
+$user_db = 'root';
+$pass_db = '';
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=db_geniuseg', $user_db, $pass_db);
+
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+$genre = $_POST['username'];
+$email = $_POST['password'];
+$confirmation_email = $_POST['passwordconf'];
+$numero = $_POST['email'];
+$pays = $_POST['securityq'];
+$niveau_etude = $_POST['qanswer'];
+$thematique = $_POST['securityq'];
+$choix_campuse = $_POST['qanswer'];
+
+if(empty($prenom) || empty($nom) || empty($genre) || empty($email) || empty($passwordconf) || empty($email) || empty($securityq) || empty($qanswer))
+{
+    $req= $dbh -> prepare('INSERT INTO db_geniuseg (name, message_post, priorite, type) VALUES(?, ?, ?, ?)');
+    $req -> execute([$nom, $message_post, $priorite, $type]);
+
+        // header('location:contacts-table.php');
+
+    echo "You did not fill out the required fields.";
+}
+
+
 
 ?>
